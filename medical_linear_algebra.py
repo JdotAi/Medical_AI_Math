c3e8c9e193d2e3efaa43defec3920eb20a16d840
Jay Patel <jpatel625@gatech.edu>
2025-08-01 10:55:37 -0400

    matrix multiply_DONE

--- medical_linear_algebra.py
+++ medical_linear_algebra.py
@@ -7,8 +7,13 @@ def matrix_multiply(A, B):
     if len(A[0])!= len(B):
         print("The matrix size is not right, check metrix size")
 
-    result = [ [0 for _ in range(len(B))] for _ in range(len(A[0]))]
+    result = [ [0 for _ in range(len(B[0]))] for _ in range(len(A))]
     
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k]* B[k][j]
+    return result
 
 
     
